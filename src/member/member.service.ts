5164c6185f6463cfb985067d318699b4442db6c5
sam <samliao1@auforce.com>
2024-04-17 15:30:15 +0800

    ✨ feat: 新增半成品的 jwt token

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConnectionPool, Request, config } from 'mssql';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class MemberService {
@@ -31,7 +32,77 @@ export class MemberService {
 
     await pool.close();
 
-    return result.recordset; // or result.returnValue depending on your SP
+    const errorMessage: Map<number, string> = new Map([
+      [1, '正常'],
+      [-1, '其它錯誤'],
+      [-2, '帳號或密碼錯誤'],
+      [-4, '會員在線'],
+      [-5, '停用'],
+      [-6, '會員鎖定'],
+      [-7, '會員凍結'],
+      [-8, '無此帳號'],
+      [-9, '組別不匹配'],
+    ]);
+
+    let message = '';
+    if (errorMessage.has(result.recordset[0].ErrorCode)) {
+      // 鍵存在於 map 中
+      message = errorMessage.get(result.recordset[0].ErrorCode);
+    }
+
+    if (result.recordset[0].ErrorCode === 1) {
+      const token = jwt.sign(
+        {
+          password: data.password,
+          username: data.account,
+          clubid: result.recordset[0].clubid,
+          clubename: result.recordset[0].clubename,
+          clubcname: result.recordset[0].clubcname,
+          // franchiserid (待補上)
+          zuBie: result.recordset[0].ZuBie,
+          huilvtype: result.recordset[0].huilvtype,
+          // forwardGameCurrency (待補上)
+          // site_id (待補上)
+          // timestamp (待補上)
+          isSocialLogin: false,
+          union_id: result.recordset[0].Union_id,
+          unitKey: result.recordset[0].UnitKey,
+        },
+        'de495842bf7196a10db5d3df43fefee9d1b67e43eeb4e02dd074cd4ab83ece59',
+        { expiresIn: '1h' },
+      ); // token will expire in 1 hour
+      return {
+        status: 1,
+        desc: 'SUCCESS',
+        result: {
+          token: token,
+          clubid: result.recordset[0].clubid,
+          clubename: result.recordset[0].clubename,
+          clubcname: result.recordset[0].clubcname,
+          huilvtype: result.recordset[0].huilvtype,
+          now_xinyong: result.recordset[0].now_xinyong,
+          isTest: result.recordset[0].IsTest,
+          zuBie: result.recordset[0].ZuBie,
+          isSocialLogin: false,
+          collet: result.recordset[0].Collet,
+          payment: result.recordset[0].Payment,
+          collet_isEnabled: result.recordset[0].Collet_isEnabled,
+          payment_isEnabled: result.recordset[0].Payment_isEnabled,
+          expiresAt: 1713930586,
+          mustChangePasssword: result.recordset[0].mustChangePasssword,
+          union_id: result.recordset[0].Union_id,
+          unitKey: result.recordset[0].UnitKey,
+        },
+        errorDetail: null,
+      };
+    } else {
+      return {
+        status: 0,
+        desc: 'FAILURE',
+        result: null,
+        errorDetail: message,
+      };
+    }
   }
   /**
    * 取得玩家資訊
