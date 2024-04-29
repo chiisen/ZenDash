8f4e4366c2623e26885b172a480ec65adb853661
sam <samliao1@auforce.com>
2024-04-29 11:34:22 +0800

    💄 optimize: 調整 token 檢查到 middeware 目錄

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MemberController } from './member/member.controller';
 import { MemberService } from './member/member.service';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
-import { HttpStrategy } from './http.strategy';
+import { HttpStrategy } from './middleware/http.strategy';
 import { GameController } from './game/game.controller';
 import { GameService } from './game/game.service';
 import { ExampleController } from './example/example.controller';
